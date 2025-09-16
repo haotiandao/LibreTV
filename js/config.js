5103e704b9244a8a8b72b1c8a51a957ccc475827
haotiandao <haotianwuji@hotmail.com>
2025-09-16 14:23:11 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,9 +22,10 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+   "dyttzy": {
+      "api": "http://caiji.dyttzyapi.com/api.php/provide/vod",
+      "name": "电影天堂资源",
+      "detail": "http://caiji.dyttzyapi.com",
         adult: true
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
