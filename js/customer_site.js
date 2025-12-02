7ab9e2b5245cb8214461deafd8266d7e41521c62
haotiandao <haotianwuji@hotmail.com>
2025-12-02 16:20:16 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,17 +1,17 @@
 const CUSTOMER_SITES = {
-   'dyttzy': {
+   dyttzy: {
       api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
       name: '电影天堂资源',
       'detail': 'http://caiji.dyttzyapi.com',
       'is_adult': false
     },
-    'heimuer': {
+    heimuer: {
       api: 'https://json.heimuer.xyz/api.php/provide/vod',
       name: '黑木耳',
       'detail': 'https://heimuer.tv',
       'is_adult': false
     },
-    'ruyi': {
+    ruyi: {
       api: 'https://cj.rycjapi.com/api.php/provide/vod',
       name: '如意资源',
       'is_adult': false
